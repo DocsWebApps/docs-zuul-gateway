d29e79fe723694f20d012b095e2523cb0ff9807c
Dave.Collier <lardelbow@gmail.com>
2019-11-27 21:14:14 +0000

    Initial commit

---
+++ src/main/java/com/docswebapps/docszuulgateway/DocsZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.docswebapps.docszuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsZuulGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docswebapps/docszuulgateway/DocsZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docswebapps.docszuulgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
